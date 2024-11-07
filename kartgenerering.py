22c5d22d45b748ecd105d3123f563e3a7573e515
Anton Bergström <60569011+aber007@users.noreply.github.com>
2024-11-07 11:19:40 +0100

    Create kartgenerering.py

---
+++ kartgenerering.py
@@ -0,0 +1,35 @@
+"""
+Tom = 0, 0.2
+Monster = 1, 0.4
+Kista = 2, 0.2
+Fälla = 3, 0.1
+Shop = 4, 0.1
+"""
+def createmap(size):
+    if size % 2 == 0:
+        raise ValueError ("Size måste vara ojämn")
+    typer = [0,1,2,3,4]
+    sannolikhet = [0.2,0.4,0.2,0.1,0.1]
+    from random import choices
+
+
+
+    map = [[0 for x in range(size)] for y in range(size)]
+
+    map[0][1] = 1
+    for x in range(size):
+        for y in range(size):
+            if x == int(size/2+1)-1 and y == int(size/2+1)-1:
+                map[x][y] = 0
+            else:
+                typ = choices(typer, sannolikhet)
+                map[x][y] = typ[0]
+                print(typ)
+            
+    return map
+    
+if __name__ == "__main__":
+    size = 5
+    map = createmap(size)
+    for i in range(size):
+        print(map[i])
