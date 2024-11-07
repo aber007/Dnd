26873fb195336280c0bd8375e3a1013173ba57f5
Anton Bergström <60569011+aber007@users.noreply.github.com>
2024-11-07 10:42:25 +0100

    init

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("Hello world")
-print("boing")
+"""
+init karta och UI karta
+Titel
+
+
+
+while True:
+    input = Vad vill du göra
+    if movement:
+        Byt rum och starta vad som händer i rum
+        if rum = monster:
+            starta fight
+        if rum = kista:
+            ge item
+        if rum = ...
+    if inventory:
+        visa inventory
+        input = byt / släng saker
+
+"""
