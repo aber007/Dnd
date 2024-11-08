ad90c3ae8e27db331a7580ac974203ff11344833
temp127389127389 <theodor.wallinder@skola.taby.se>
2024-11-09 00:43:01 +0100

    combat, traps, mimics, room init, room interaction

--- DnD/__init__.py
+++ DnD/__init__.py
@@ -1,9 +1,17 @@
 CONSTANTS = {
     "map_base_size": 9,
+    "trap_base_dmg": 3,
+
     "player_base_inventory_size": 3,
-    "player_base_hp": 6,
+    "player_base_hp": 10,
+    
     "enemy_base_hp": 4,
     "enemy_base_dmg": 2,
+    
+    "dice_base_sides": 20,
+    "normal_trap_base_min_roll_to_escape": 10,
+    "mimic_trap_base_ambush_dmg": 4,
+    
     "items_config_file": "./items.json"
 }
 
