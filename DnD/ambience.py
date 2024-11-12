7db1c3ecfdb44744f7c2c5cd90f0c012bfba28ae
Eskil Albertsson <eskil.albertsson@skola.taby.se>
2024-11-12 11:58:10 +0100

    changes to music

--- DnD/ambience.py
+++ DnD/ambience.py
@@ -12,20 +12,36 @@ except:
 # Initialize pygame mixer
 pygame.mixer.init()
 
-def play(file_path):
-    if CONSTANTS["music"]:
-        current_dir = os.path.dirname(__file__) if '__file__' in globals() else os.getcwd()
-        target_file = os.path.join(current_dir, '..', 'story', file_path)
-        target_file = os.path.abspath(target_file)
-        pygame.mixer.music.load(target_file)
-        pygame.mixer.music.play(loops=-1)
+class music:
 
-def pause():
-    pygame.mixer.music.pause()
+    def __init__(self, ambience_playlist):
+        self.ambience_playlist = ["../story/test.wav", "../story/fight.mp3"]
 
-def resume():
-    pygame.mixer.music.unpause()
+    def play(file_path):
+        if CONSTANTS["music"]:
+            current_dir = os.path.dirname(__file__) if '__file__' in globals() else os.getcwd()
+            target_file = os.path.join(current_dir, '..', 'story', file_path)
+            target_file = os.path.abspath(target_file)
+            pygame.mixer.music.load(target_file)
+            pygame.mixer.music.play(loops=-1)
 
-def stop():
-    pygame.mixer.music.stop()
+    def nexttrack(self):
+        global index
+        index += 1
+        if index >= len(self.ambience_playlist):
+            index = 0
+        pygame.mixer.music.load(self.ambience_playlist[index])
+        pygame.mixer.music.play()
+
+    def pause(self):
+        pygame.mixer.music.pause()
+
+    def resume(self):
+        pygame.mixer.music.unpause()
+
+    def stop(self):
+        pygame.mixer.music.stop()
+
+    def fadeout(self):
+        pygame.mixer.music.fadeout()
 
