95ce4441de868c915c5d4974ff5ea151adfeb270
temp127389127389 <theodor.wallinder@skola.taby.se>
2024-11-15 21:23:54 +0100

    Revert "start of music auto play"
    This reverts commit 1b556085931ce914fa3190401ef83ed261cf5496.

--- DnD/ambience.py
+++ DnD/ambience.py
@@ -1,7 +1,7 @@
 #This does not need to be in the final game, i was just bored
 
 from . import CONSTANTS, Slider
-from random import shuffle
+from random import choice
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 try:
@@ -15,63 +15,71 @@ pygame.mixer.init()
 
 class Music:
 
-    def __init__(self): # "ambience2.mp3", 
-        self.ambience = ["ambience1.mp3", "ambience3maybe.mp3", "ambience4.mp3", "ambience5.mp3"] ; shuffle(self.ambience)
+    def __init__(self):
+        self.ambience = ["ambience1.mp3", "ambience2.mp3", "ambience3maybe.mp3", "ambience4.mp3", "ambience5.mp3"]
+        self.replace_ambience = []
         self.fight = "fight.mp3"
         self.shop = "shop_music.mp3"
         self.file_path = ""
         self.current_song = ""
 
-        self.current_dir = os.path.dirname(__file__) if '__file__' in globals() else os.getcwd()
-        self.story_dir = os.path.abspath(os.path.join(self.current_dir, '..', 'story'))
 
-        if CONSTANTS["music_enabled"]:
-            pygame.mixer.music.set_volume(0)
-            self.play("ambience")
+        pygame.mixer.music.set_volume(0)
+        self.play("ambience")
 
-            # the +1 adds a step where the volume is 0
-            slider_steps = round(1 / CONSTANTS["music_slider_step_volume_percent"]) + 1
-            slider_on_value_changed = lambda val : pygame.mixer.music.set_volume(max(0, val/slider_steps * CONSTANTS["music_max_volume_percent"]))
-            Slider(slider_steps, slider_on_value_changed, header="Choose music volume").start()
+        # the +1 adds a step where the volume is 0
+        slider_steps = round(1 / CONSTANTS["music_slider_step_volume_percent"]) + 1
+        slider_on_value_changed = lambda val : pygame.mixer.music.set_volume(max(0, val/slider_steps * CONSTANTS["music_max_volume_percent"]))
+        Slider(slider_steps, slider_on_value_changed, header="Choose music volume").start()
 
-
-    def start_auto_queue():
-        pass
 
     def play(self, type):
-        if not CONSTANTS["music_enabled"]:
-            return
-        
-        match type:
-            case "ambience":
-                # if the current song is of type ambience AND the current song isnt the last song in the ambience list:
-                #   play the next song
-                if self.current_song in self.ambience and self.current_song != self.ambience[-1]:
-                    current_song_idx = self.ambience.index(self.current_song)
-                    self.file_path = self.ambience[current_song_idx+1]
+        if CONSTANTS["music"]:
+            match type:
+                case "ambience":
+                    if len(self.ambience) != 0:
+                        self.file_path = choice(self.ambience)
+                        self.replace_ambience.append(self.file_path)
+                        self.ambience.pop(self.ambience.index(self.file_path))
+                    else:
+                        self.ambience = self.replace_ambience.copy()
+                        self.replace_ambience.clear()
+                        self.file_path = choice(self.ambience)
+                        self.replace_ambience.append(self.file_path)
+                        self.ambience.pop(self.ambience.index(self.file_path))
+                case "fight":
+                    self.file_path = self.fight
+                case "shop":
+                    self.file_path = self.shop
 
-                # otherwise if the current song isnt of type ambience OR we've ran out of songs:
-                #   reshuffle the song list and play the first one
-                else:
-                    shuffle(self.ambience)
-                    self.file_path = self.ambience[0]
-            
-            case "fight":
-                self.file_path = self.fight
-            
-            case "shop":
-                self.file_path = self.shop
+            current_dir = os.path.dirname(__file__) if '__file__' in globals() else os.getcwd()
+            target_file = os.path.join(current_dir, '..', 'story', self.file_path)
+            target_file = os.path.abspath(target_file)
+            pygame.mixer.music.load(target_file)
+            if type == "ambience":
+                pygame.mixer.music.play(loops=0, fade_ms=500)
+                self.nexttrack()
+            else:
+                pygame.mixer.music.play(loops=-1, fade_ms=500)
+            self.current_song = self.file_path
 
-        
-        target_file = os.path.join(self.story_dir, self.file_path)
-        pygame.mixer.music.load(target_file)
-
-        if type == "ambience":
-            pygame.mixer.music.play(loops=0, fade_ms=500)
+    def nexttrack(self):
+        if len(self.ambience) != 0:
+            self.file_path = choice(self.ambience)
+            self.replace_ambience.append(self.file_path)
+            self.ambience.pop(self.ambience.index(self.file_path))
         else:
-            pygame.mixer.music.play(loops=-1, fade_ms=500)
-        
-        self.current_song = self.file_path
+            self.ambience = self.replace_ambience.copy()
+            self.replace_ambience.clear()
+            self.file_path = choice(self.ambience)
+            self.replace_ambience.append(self.file_path)
+            self.ambience.pop(self.ambience.index(self.file_path))
+
+        current_dir = os.path.dirname(__file__) if '__file__' in globals() else os.getcwd()
+        target_file = os.path.join(current_dir, '..', 'story', self.file_path)
+        target_file = os.path.abspath(target_file)
+
+        pygame.mixer.music.queue(target_file)
 
     def pause(self):
         pygame.mixer.music.pause()
